38b64291e14f91b83ce5ce1099146bc078080958
Phuc <hahoangphuc281@gmail.com>
2026-01-12 19:35:19 +0700

    feat: update order product

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,12 +1,14 @@
+// File: routes/productRoutes.js
 import express from 'express';
 import ProductController from '../controllers/productController.js';
 
 const router = express.Router();
 
-// Route cũ
+// Route lấy danh sách: http://localhost:3001/api/products
 router.get('/', ProductController.getAllProducts);
 
-// Route MỚI: lấy chi tiết
+// Route lấy chi tiết + đánh giá (MỚI THÊM)
+// Gọi kiểu: http://localhost:3001/api/products/detail/1
 router.get('/detail/:id', ProductController.getProductDetail);
 
 export default router;
